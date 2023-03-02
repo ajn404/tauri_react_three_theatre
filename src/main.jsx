0bc1a2360b9e026a2aa305dd49c2f45909f22b55
mac_ajn404 <huiyuening00@gmail.com>
2023-03-02 16:12:00 +0800

    no message

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
 
+React.useEffect = React.useLayoutEffect;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
